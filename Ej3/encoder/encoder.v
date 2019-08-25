60a22f16863328a13c1fe333a2d2d77e4441c401
gonzadavidov <gdavidov@itba.edu.ar>
2019-08-25 06:41:02 -0300

    Modificado ej 3 encoder

--- Ej3/encoder/encoder.v
+++ Ej3/encoder/encoder.v
@@ -10,7 +10,7 @@ begin
     case(in)
     0000 : assign out = 2'bx;
     4'b0001 : assign out = 0;
-    4'b0010 :  out = 1;
+    4'b0010 : assign out = 1;
     4'b0100 : assign out = 2;
     4'b1000 : assign out = 3;
     default assign out = 2'bZZ;
